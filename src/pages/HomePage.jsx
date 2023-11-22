2b6adbc81f9193500adfcfd81584f5813bee5f52
YoungJ <wns4011@daum.net>
2023-11-22 11:07:20 +0900

    홈페이지 로그인 연동

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,6 +1,11 @@
-import React from 'react';
+import React, {useEffect} from 'react';
+import {getAuth} from 'firebase/auth';
 
 const HomePage = () => {
+  useEffect(() => {
+    // 로그인되어 있으면 다시 메인으로..
+    console.log(getAuth().currentUser);
+  }, []);
   return <div></div>;
 };
 
