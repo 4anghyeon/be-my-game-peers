cc7043940a8aa8e31e08d01e501648bd4a686a86
im2hw <julisanta1015@gmail.com>
2023-11-22 15:38:55 +0900

    UserDetailPage로 이동

--- src/pages/UserDetailPage.jsx
+++ src/pages/UserDetailPage.jsx
@@ -1,24 +1,187 @@
 import React from 'react';
-import UserInfo from 'components/User/UserInfo';
-import UserComments from 'components/User/UserComments';
 import styled from 'styled-components';
+import fakeData from 'db/fakeData.json';
+import avatar from 'assets/avatar.png';
+import uuid from '../../../node_modules/react-uuid/uuid';
+import {useState, useEffect} from 'react';
+import {getAuth} from 'firebase/auth';
 
 const UserDetailPage = () => {
+  const [userInfo, setUserInfo] = useState(fakeData);
+  const [nickname, setNickName] = useState('');
+  const [about, setAbout] = useState('');
+  const [favoriteGame, setFavoriteGame] = useState('');
+
+  const [isEdit, setIsEdit] = useState(false);
+
+  const EDIT_NICKNAME = event => setNickName(event.target.value);
+  const EDIT_ABOUT = event => setAbout(event.target.value);
+  const EDIT_FAVORITE = event => setFavoriteGame(event.target.value);
+
+  let NEW_USER_INFO = {};
+
+  const EDIT_BUTTON = () => {
+    setIsEdit(!isEdit);
+    NEW_USER_INFO = {
+      id: uuid(),
+      nickname,
+      about,
+      favoriteGame,
+    };
+
+    console.log(NEW_USER_INFO);
+    setUserInfo(userInfo => [NEW_USER_INFO, ...userInfo]);
+
+    setNickName('');
+    setAbout('');
+    setFavoriteGame('');
+  };
+
+  useEffect(() => {
+    console.log(getAuth().currentUser);
+  }, []);
+
+  const choosePhoto = () => {};
+
   return (
-    <Container>
-      <UserInfo />
-      <UserComments />
-    </Container>
+    <>
+      <ScContainer>
+        <ScSelectImg>
+          <ScProfileImg src={avatar} alt="프로필 이미지" />
+          {isEdit ? <ScSelectPictureBtn>사진 선택</ScSelectPictureBtn> : null}
+        </ScSelectImg>
+        <ScInfoBox>
+          {isEdit ? (
+            <ScInput type="text" value={nickname} onChange={EDIT_NICKNAME} placeholder="닉네임" />
+          ) : (
+            <ScUserName>{userInfo[0].nickname}님</ScUserName>
+          )}
+        </ScInfoBox>
+        <ScInfoBox>
+          <ScLabel>About</ScLabel>
+          {isEdit ? (
+            <ScInput type="text" value={about} onChange={EDIT_ABOUT} placeholder="한줄 소개" />
+          ) : (
+            <ScAbout>{userInfo[0].about}</ScAbout>
+          )}
+        </ScInfoBox>
+        <ScInfoBox>
+          <ScLabel>Favorite Game</ScLabel>
+          {isEdit ? (
+            <ScInput type="text" value={favoriteGame} onChange={EDIT_FAVORITE} placeholder="좋아하는 게임" />
+          ) : (
+            <ScAbout>{userInfo[0].favoriteGame}</ScAbout>
+          )}
+        </ScInfoBox>
+        <ScBtnBox>
+          <ScButton onClick={EDIT_BUTTON}>{isEdit ? 'save' : 'edit'}</ScButton>
+          <ScButton>내 게시물</ScButton>
+        </ScBtnBox>
+      </ScContainer>
+    </>
   );
 };
 
-const Container = styled.div`
+const ScContainer = styled.div`
+  color: white;
   display: flex;
   flex-direction: column;
-  width: 100%;
-  height: 100vh;
-  background-color: white;
+  width: 1200px;
   align-items: center;
+  margin: 20px auto;
+  padding: 12px;
+`;
+
+const ScSelectImg = styled.div`
+  width: 600px;
+  display: flex;
+  flex-direction: row;
+  align-items: flex-end;
+  justify-content: flex-start;
+  gap: 20px;
+  padding: 12px 6px;
+`;
+
+const ScProfileImg = styled.img`
+  width: 180px;
+  height: 180px;
+  border-radius: 50%;
+`;
+
+const ScSelectPictureBtn = styled.button`
+  background-color: #7752fe;
+  color: white;
+  width: 100px;
+  height: 36px;
+  border-radius: 10px;
+  border: none;
+`;
+
+const ScInfoBox = styled.div`
+  width: 600px;
+  margin: 20px auto;
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+  justify-content: center;
+  color: black;
+  gap: 10px;
+`;
+
+const ScUserName = styled.h2`
+  font-size: 2rem;
+  font-weight: 700;
+`;
+
+const ScAbout = styled.p`
+  display: flex;
+  align-items: center;
+  font-size: 1rem;
+  font-weight: 500;
+  background-color: #eee;
+  padding: 6px;
+  border-radius: 10px;
+  width: 600px;
+  height: 36px;
+`;
+
+const ScLabel = styled.label`
+  font-size: 0.9rem;
+  color: #7752fe;
+  font-weight: 700;
+`;
+
+const ScInput = styled.input`
+  width: 600px;
+  height: 30px;
+  border-radius: 10px;
+  border: 1px solid #ddd;
+  color: #333;
+  padding: 4px;
+`;
+
+const ScBtnBox = styled.div`
+  width: 600px;
+  display: flex;
+  margin: 0 auto;
+  gap: 12px;
+  margin-top: 20px;
+  justify-content: flex-end;
+`;
+
+const ScButton = styled.button`
+  background-color: white;
+  font-weight: 600;
+  color: black;
+  width: 80px;
+  height: 36px;
+  border-radius: 10px;
+  border: 2px solid #7752fe;
+
+  &:hover {
+    background-color: #7752fe;
+    color: white;
+  }
 `;
 
 export default UserDetailPage;
