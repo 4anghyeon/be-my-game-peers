ff2806237a47946ab622ab1ea54411a0c5b35a00
sanghyeon <uphyeon@kakao.com>
2023-11-25 01:21:35 +0900

    댓글 수정

--- src/pages/MyPostPage.jsx
+++ src/pages/MyPostPage.jsx
@@ -6,6 +6,7 @@ import {auth} from '../shared/firebase/firebase';
 import {useNavigate} from 'react-router-dom';
 import {useAlert} from '../redux/modules/alert/alertHook';
 import moment from 'moment/moment';
+import MyPostListContainer from '../components/UserDetail/MyPostListContainer';
 
 export default function MyPost() {
   const [currentUserEmail, setCurrentUserEmail] = useState('');
@@ -15,18 +16,23 @@ export default function MyPost() {
   const navigate = useNavigate();
   const posts = useSelector(state => state.PostModule);
 
-  // TODO: comments!!
-  const comments = useSelector(state => state.PostModule);
-  console.log(currentUserEmail);
-  console.log(
-    posts.reduce((acc, cur) => {
-      console.log(cur.comments);
-      return [...acc, ...cur.comments.filter(c => c.userEmail === currentUserEmail)];
-    }, []),
-  );
+  const comments = posts.reduce((acc, cur) => {
+    const filtered = cur.comments
+      .filter(c => c.userEmail === currentUserEmail)
+      .map(c => {
+        return {
+          postId: cur.postId,
+          postTitle: cur.postTitle,
+          category: cur.category,
+          postDate: cur.postDate,
+          ...c,
+        };
+      });
+    if (filtered.length > 0) return [...acc, ...filtered];
+    return acc;
+  }, []);
 
   const currentUser = auth.currentUser;
-  const selectedComments = comments.filter(comment => comment.commentId);
 
   useEffect(() => {
     if (!currentUser) {
@@ -39,43 +45,20 @@ export default function MyPost() {
     }
   }, []);
 
-  const onClickPost = id => {
-    navigate(`/detail/${id}`);
-  };
-
   // 내 게시물
   return (
     <CenterVertical>
       <ScWrapper>
         <h3>내가 쓴 게시물 ({selectedPost.length})</h3>
-        <ScMyPost>
-          {selectedPost.map(list => {
-            return (
-              <li key={list.id} onClick={onClickPost.bind(null, list.postId)}>
-                <h4 className="title">[{list.category}]</h4>
-                <h4 className="title">{list.postTitle}</h4>
-                <h4 className="title">{moment.unix(list.postDate.seconds).format('yyyy-MM-DD HH:mm')}</h4>
-              </li>
-            );
-          })}
-        </ScMyPost>
+        <MyPostListContainer list={selectedPost} />
       </ScWrapper>
       <ScWrapper className="myComment">
-        <h3>내가 쓴 댓글</h3>
-        <ScMyPost>
-          {selectedComments.length === 0
-            ? `${currentUserEmail.displayName}님이 작성한 댓글이 없습니다.`
-            : comments
-                .filter(comment => comment.authorEmail === currentUserEmail)
-                .map(list => {
-                  return (
-                    <li key={list.id}>
-                      <p className="content">{list.content}</p>
-                      <p className="writer">작성자 : {list.userId}</p>
-                    </li>
-                  );
-                })}
-        </ScMyPost>
+        <h3>내가 쓴 댓글 ({comments.length})</h3>
+        {comments.length === 0 ? (
+          `${currentUserEmail.displayName}님이 작성한 댓글이 없습니다.`
+        ) : (
+          <MyPostListContainer list={comments} />
+        )}
       </ScWrapper>
     </CenterVertical>
   );
@@ -90,40 +73,5 @@ const ScWrapper = styled.div`
   h3 {
     color: #7752fe;
     font-size: 1.2rem;
-  }
-`;
-
-const ScMyPost = styled.div`
-  display: flex;
-  flex-direction: column;
-  background-color: white;
-  height: 100%;
-  padding: 10px;
-  gap: 12px;
-
-  li {
-    display: flex;
-    align-items: center;
-    justify-content: space-between;
-    gap: 20px;
-    width: 100%;
-    height: 40px;
-    border: 1px solid lightgrey;
-    border-radius: 5px;
-    padding: 10px;
-    cursor: pointer;
-
-    .title {
-      width: 200px;
-    }
-
-    .content {
-      width: 300px;
-    }
-
-    .writer {
-      font-size: 14px;
-      color: #333;
-    }
   }
 `;
